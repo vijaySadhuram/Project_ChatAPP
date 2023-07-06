afd9b50e797b165ec8ca67ff51e7e141a6eecb4a
Vijay <vijaysadhuram4868@gmail.com>
2023-07-06 22:09:44 +0530

    Create message component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-
-
+// container is similar to div
+import {Button,Box,Container,VStack, Input, HStack} from "@chakra-ui/react"
+import Message from "./Component/Message";
 function App() {
   return (
-    <div className="App">
+   <Box  bg={"red.50"}>
+    <Container bg={"white"} h={"100vh"}>
+
+      {/* Display:flex  flex-direction:column*/}
+    <VStack  h={"full"} paddingY={"4"}>
+
+      <Button  colorScheme="red" w={"full"}>Logout</Button>
+      {/* Message holder */}
+
+      <VStack  h={"full"} w={"full"}>
+     <Message  text={"sample one"}/>
       
-    </div>
+      </VStack>
+      {/* form */}
+
+      <form   style={{width:"100%"}}>
+      <HStack>
+      <Input placeholder="Enter a Message...." colorScheme="white" w={"full"}></Input>
+      <Button type="submit" colorScheme="purple">Send</Button>
+      
+      </HStack>
+      </form>
+      
+    </VStack>
+    
+    </Container>
+
+   </Box>
   );
 }
 
---
+++ src/Component/Message.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {HStack,Avatar,Text} from "@chakra-ui/react"
+
+const 
+message = ({text,uri}) => {
+  return (
+    <HStack>
+        <Text>
+            {text}
+        </Text>
+        <Avatar src='{uri}'></Avatar>
+    </HStack>
+  )
+}
+
+export default 
+message
