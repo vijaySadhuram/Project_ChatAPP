76104416e45555994504a9b5e9319d031e640b81
Vijay <vijaysadhuram4868@gmail.com>
2023-07-12 12:35:28 +0530

    get Auth

--- src/App.js
+++ src/App.js
@@ -1,36 +1,113 @@
 // container is similar to div
-import {Button,Box,Container,VStack, Input, HStack} from "@chakra-ui/react"
-import Message from "./Component/Message";
+import { Button, Box, Container, VStack, Input, HStack } from "@chakra-ui/react"
+import Message from "./Component/Message"
+import {onAuthStateChanged, getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
+import { app } from "./Firebase"
+import { useState ,useEffect} from "react"
+import {getFirestore,addDoc, collection, serverTimestamp,onSnapshot} from "firebase/firestore"
+
+const auth = getAuth(app);
+const database=getFirestore(app);
+
+
+// console.log(auth);
+
+
+
+const loginHandler = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider);
+}
+const logoutHandler=()=>{
+  signOut(auth);
+}
 function App() {
-  return (
-   <Box  bg={"red.50"}>
-    <Container bg={"white"} h={"100vh"}>
 
-      {/* Display:flex  flex-direction:column*/}
-    <VStack  h={"full"} paddingY={"4"}>
+  const [user, setUser] = useState(false);
+  const [message,setmessage]=useState("");
+  const [messages,setMessages]=useState([]);
 
-      <Button  colorScheme="red" w={"full"}>Logout</Button>
-      {/* Message holder */}
 
-      <VStack  h={"full"} w={"full"}>
-     <Message  text={"sample one"}/>
-      
-      </VStack>
-      {/* form */}
+  const submitHandler=async (e)=>{
+    e.preventDefault();
+   try {
+    await addDoc(collection(database,"Messagess"),{
+      text:message,
+      uid: user.uid,
+      uri:user.photoURL,
+      createdAt:serverTimestamp()
+    });
+    setmessage("");
+   } catch (error) {
 
-      <form   style={{width:"100%"}}>
-      <HStack>
-      <Input placeholder="Enter a Message...." colorScheme="white" w={"full"}></Input>
-      <Button type="submit" colorScheme="purple">Send</Button>
-      
-      </HStack>
-      </form>
-      
-    </VStack>
+    alert(error)
     
-    </Container>
+   }
+  
+  }
 
-   </Box>
+  useEffect(() => {
+const unsubsribe  = onAuthStateChanged(auth,(data)=>{
+      // console.log(data);
+      setUser(data);  
+    });
+    const unsubscribebeformessage= onSnapshot(collection(database,"Messagess"),(snap)=>{
+     setMessages(snap.docs.map((item)=>{
+      const id=item.id;
+      return {id,...item.data()};
+     }));
+      });
+  
+    return()=>{
+      unsubsribe();
+      unsubscribebeformessage();
+    }
+  });
+
+  
+  return (
+    <Box bg={"red.50"}>
+      {
+        user ? (
+          <Container bg={"white"} h={"100vh"}>
+
+            {/* Display:flex  flex-direction:column*/}
+            <VStack h={"full"} paddingY={"4"}>
+
+              <Button onClick={logoutHandler} colorScheme="red" w={"full"}>Logout</Button>
+              {/* Message holder */}
+
+              <VStack h={"full"} w={"full"} overflowY={"auto"}  >
+                {
+                messages.map(item=>(
+
+              <Message 
+              key={item.id}
+              text={item.text} uri={item.uri} user={item.uid===user.uid?"me" : "other"} />
+              ))
+                }
+              </VStack>
+              {/* form */}
+
+              <form style={{ width: "100%" }}   onSubmit={submitHandler}>
+                <HStack>
+                  <Input value={message} onChange={(e)=>setmessage(e.target.value)} placeholder="Enter a Message...." colorScheme="white" w={"full"}></Input>
+                  <Button type="submit" colorScheme="purple">Send</Button>
+
+                </HStack>
+              </form>
+
+            </VStack>
+
+          </Container>
+
+        ) :
+          (<VStack alignItems={"center"} h={"100vh"} justifyContent={"center"}>
+            <Button colorScheme="blue" onClick={loginHandler}>Sign In With Google</Button>
+          </VStack>
+          )
+      }
+    </Box>
   );
 }
 
